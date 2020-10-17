1cccbf7a3ec9e69739d309aaf5e67732b7b8f04d
Mannsha <72664766+Mannsha@users.noreply.github.com>
2020-10-17 12:01:08 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C20-BounceOff-Algorithm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,38 @@
+var movingRect, fixedRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  movingRect= createSprite(400, 200, 80, 50);
+  fixedRect= createSprite(600, 200, 50, 80);
+  movingRect.shapeColor="green";
+  fixedRect.shapeColor="green";
+  movingRect.debug=true;
+  fixedRect.debug=true;
+  movingRect.velocityX=8;
+  fixedRect.velocityX=-8;
+}
+
+function draw() {
+  background(0,0,0);  
+
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2
+    &&fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2)
+    {
+      movingRect.velocityX=movingRect.velocityX*(-1);
+      fixedRect.velocityX=fixedRect.velocityX*(-1);
+    }
+    
+  if(movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2
+    &&fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2)
+    {
+      movingRect.velocityY=movingRect.velocityY*(-1);
+      fixedRect.velocityY=fixedRect.velocityY*(-1);
+    }
+
+
+
+
+
+  drawSprites();
+}
